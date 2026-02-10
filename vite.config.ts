d79d89c7879544d10eeec9d9d6deb8b25fa6e0f7
ShreyaPandit <shreya.pandit@ringcentral.com>
2026-02-10 12:24:09 +0530

    my last commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/NewTictactoe/', // required for GitHub Pages (replace with your repo name if different)
 })
